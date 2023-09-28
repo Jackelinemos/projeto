5dc313c24907d38d4fea3cca4d077244dc6e99f5
JackelineMo <jackelineradcliffe1@gmail.com>
2023-09-28 09:16:22 -0400

    Atualizações

---
+++ python/codigo.py
@@ -0,0 +1,14 @@
+# Passo a passo do projeto
+# Passo 1: Entrar no sistemaa empresa
+    #https://www.hashtagtreinamentos.com
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados de produtos
+# Passo 4:Cadastrar 1 produto
+# Passo5:Repetir o cadastro para todos os produtos
+
+
+
+
+
+
+
